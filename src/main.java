a9ad9e9a5e004a1e4a1da1f00f1a3b3d8d0c25a1
De-la-Torre-Katherin <kadltg12@gmail.com>
2023-12-13 14:49:54 -0500

    exposicion2

---
+++ src/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class main {
+     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        HasSet hash=new HasSet(); 
+        hash.setVisible(true);
+    }
+}
